50e80e080350fa15b0694d50872c4846c5a1b6b3
Piotr Swis <zmorekcraftx@gmail.com>
2023-04-05 20:30:32 +0200

    prettier

--- index.js
+++ index.js
@@ -1,67 +1,52 @@
-
-
-const lineContainer = document.querySelector('.lineContainer')
+const lineContainer = document.querySelector(".lineContainer");
 const mobileLineDistance = 60;
 const lineDistance = 80;
-let isMobileMenuOpened = false
+let isMobileMenuOpened = false;
 const createLines = (count) => {
-   for (let index = 0; index < count; index++) {
-
-    const line = document.createElement('div');
-line.classList.add("line")
-lineContainer.appendChild(line)
-
-   }
-
-}
+  for (let index = 0; index < count; index++) {
+    const line = document.createElement("div");
+    line.classList.add("line");
+    lineContainer.appendChild(line);
+  }
+};
 const getLines = () => {
-    lineContainer.replaceChildren()
-    const w = window.innerWidth;
+  lineContainer.replaceChildren();
+  const w = window.innerWidth;
 
+  const lineCount = Math.floor(
+    w / (w > 500 ? lineDistance : mobileLineDistance)
+  );
+  createLines(lineCount);
+};
 
-const lineCount = Math.floor( w / (w > 500 ? lineDistance : mobileLineDistance) )
-createLines( lineCount)
+getLines();
 
+window.addEventListener("resize", () => {
+  getLines();
+});
 
-}
-
-getLines()
-
-window.addEventListener('resize', () => {
-getLines()
-
-})
-
-
-
-const mobileMenu = document.querySelector('.header nav .innerContainer') 
-const openMobileButton = document.querySelector('.header nav .menuButton button.open')
-const closeMobileButton = document.querySelector('.header nav .menuButton button.close')
+const mobileMenu = document.querySelector(".header nav .innerContainer");
+const openMobileButton = document.querySelector(
+  ".header nav .menuButton button.open"
+);
+const closeMobileButton = document.querySelector(
+  ".header nav .menuButton button.close"
+);
 // const closeMobileButton = document.querySelector('')
 
-openMobileButton.addEventListener('click', () => {
-    isMobileMenuOpened = true
-  
- 
-    
+openMobileButton.addEventListener("click", () => {
+  isMobileMenuOpened = true;
 
-    mobileMenu.style.left = 0
-    openMobileButton.style.display = 'none'
-    closeMobileButton.style.display = 'block'
-    
-})
+  mobileMenu.style.left = 0;
+  openMobileButton.style.display = "none";
+  closeMobileButton.style.display = "block";
+});
 
+closeMobileButton.addEventListener("click", () => {
+  isMobileMenuOpened = false;
 
-closeMobileButton.addEventListener('click', () => {
-    isMobileMenuOpened = false
+  mobileMenu.style.left = "100%";
 
-    mobileMenu.style.left = '100%'
-
-    openMobileButton.style.display = 'block'
-    closeMobileButton.style.display = 'none'
-    
-
- 
-
-    
-})
+  openMobileButton.style.display = "block";
+  closeMobileButton.style.display = "none";
+});
